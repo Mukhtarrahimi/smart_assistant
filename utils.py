36beff2750e7d85184985c23796c43c843d96183
= <mukhtarrahimi.dev@gmail.com>
2025-09-18 20:56:56 +0430

    now_iso function

--- utils.py
+++ utils.py
@@ -4,3 +4,9 @@ from datetime import datetime
 from pathlib import Path
 
 DATA_DIR = Path(__file__).parent / 'data'
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def now_iso():
+    return datetime.now().isoformat(sep=' ', timespec='seconds')
