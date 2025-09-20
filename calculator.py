ad09b1851afb3f180283a794faac0c233b5ff04a
= <mukhtarrahimi.dev@gmail.com>
2025-09-20 21:18:19 +0430

    allowed_operator

--- calculator.py
+++ calculator.py
@@ -2,3 +2,13 @@
 import ast
 import operator as op
 import math
+
+ALLOWED_OPERATORS = {
+    ast.Add: op.add,
+    ast.Sub: op.sub,
+    ast.Mult: op.mul,
+    ast.Div: op.truediv,
+    ast.Pow: op.pow,
+    ast.Mod: op.mod,
+    ast.USub: op.neg,
+}
