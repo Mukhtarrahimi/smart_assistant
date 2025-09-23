76952941b2de73a8e71f3459a2d216f6b812ada9
= <mukhtarrahimi.dev@gmail.com>
2025-09-23 21:06:39 +0430

    import os

--- password_manager.py
+++ password_manager.py
@@ -3,3 +3,4 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
+import os
