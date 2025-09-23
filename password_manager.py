38f43a7476bb367e1d9125e158747f9ef9f4b8ea
= <mukhtarrahimi.dev@gmail.com>
2025-09-23 21:06:20 +0430

    import other

--- password_manager.py
+++ password_manager.py
@@ -1,1 +1,5 @@
 import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
