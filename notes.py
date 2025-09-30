07f514f24054b8e7702190284a619f9f5d98f791
= <mukhtarrahimi.dev@gmail.com>
2025-09-30 21:37:22 +0430

    add_note function code

--- notes.py
+++ notes.py
@@ -5,3 +5,6 @@ from utils import now_iso
 def add_note(title, content, tags=''):
     conn = get_conn()
     cur = conn.cursor()
+    t = now_iso()
+    cur.execute('INSERT INTO notes (title, content, tags, created_at, updated_at) VALUES (?, ?, ?, ?, ?)',
+    (title, content, tags, t, t))
